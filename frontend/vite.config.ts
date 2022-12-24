be9109aa8940910d3b07873ec82e46b372f2f9f9
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2022-12-24 15:34:53 +0900

    chore: yarn 設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {         // この三行を追加
-    host: true,   //
-  },                    //
+  plugins: [
+    react(),
+    tsconfigPaths(), 
+  ],
+  server: {
+    host: true,
+  },
 });
