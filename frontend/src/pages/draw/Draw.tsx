a5dbc6679074708bac1d264f3428685e3de3a6ef
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2023-03-18 14:01:41 +0900

    feat: drag and dropで位置を動かせるように機能追加

--- frontend/src/pages/draw/Draw.tsx
+++ frontend/src/pages/draw/Draw.tsx
@@ -1,9 +1,31 @@
-const Todo: React.FC = () => {
-return (
+import React, {DragEvent} from 'react';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+
+const Draw: React.FC = () => {
+
+    let x1 = 10
+    let y1 = 10
+
+    return (
         <div>
             <h2>Draw</h2>
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    <Circle
+                        x={x1}
+                        y={y1}
+                        radius={10}
+                        fill="black"
+                        draggable
+                        onDragEnd={(e: DragEvent<HTMLDivElement>) => {
+                            x1 = e.target.attrs.x
+                            y1 = e.target.attrs.y
+                        }}
+                    />
+                </Layer>
+            </Stage>
         </div>
     );
 };
 
-export default Todo;
+export default Draw;
