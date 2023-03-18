1ba737c876c783cc97d941a9c74295e5d0461b5e
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2023-03-18 14:37:58 +0900

    feat: ループで11個分黒丸を作成

--- frontend/src/pages/draw/Draw.tsx
+++ frontend/src/pages/draw/Draw.tsx
@@ -3,25 +3,85 @@ import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
 const Draw: React.FC = () => {
 
-    let x1 = 10
-    let y1 = 10
+    type Align = {
+        "x": number,
+        "y": number,
+    }
+
+    let aligns = [
+        // TODO: default値を作成
+        // TODO: backendから取得するようにする
+        {
+            "x": 100,
+            "y": 10,
+        },
+        {
+            "x": 200,
+            "y": 10,
+        },
+        {
+            "x": 300,
+            "y": 10,
+        },
+        {
+            "x": 400,
+            "y": 10,
+        },
+        {
+            "x": 500,
+            "y": 10,
+        },
+        {
+            "x": 600,
+            "y": 10,
+        },
+        {
+            "x": 700,
+            "y": 10,
+        },
+        {
+            "x": 800,
+            "y": 10,
+        },
+        {
+            "x": 900,
+            "y": 10,
+        },
+        {
+            "x": 1000,
+            "y": 10,
+        },
+        {
+            "x": 1100,
+            "y": 10,
+        },
+    ]
+
+    let circles = []
+
+    for (let i = 0; i < 10; i++) {
+        circles.push(
+            <Circle
+                x={aligns[i].x}
+                y={aligns[i].y}
+                radius={10}
+                fill="black"
+                draggable
+                onDragEnd={(e: DragEvent<HTMLDivElement>) => {
+                    aligns[i].x = e.target.attrs.x
+                    aligns[i].y = e.target.attrs.y
+                }}
+                key={i}
+            />
+        )
+    }
 
     return (
         <div>
             <h2>Draw</h2>
             <Stage width={window.innerWidth} height={window.innerHeight}>
                 <Layer>
-                    <Circle
-                        x={x1}
-                        y={y1}
-                        radius={10}
-                        fill="black"
-                        draggable
-                        onDragEnd={(e: DragEvent<HTMLDivElement>) => {
-                            x1 = e.target.attrs.x
-                            y1 = e.target.attrs.y
-                        }}
-                    />
+                    {circles}
                 </Layer>
             </Stage>
         </div>
