0bd24c333b30cc8a357ffead70475e682f9148ae
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2023-03-18 10:17:49 +0900

    feat: homeからtodoへの導線の追加

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
       <p>aaa</p>
       <h1>home</h1>
+      <ul>
+        <li>
+          <Link to="/todo">TODO</Link>
+        </li>
+      </ul>
     </div>
   );
 }
