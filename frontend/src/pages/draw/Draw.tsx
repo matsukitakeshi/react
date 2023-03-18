9c682c42b6abf9106f47fa1843c003930446f950
matsuki_takeshi <takeshi.matsuki@genialtech.io>
2023-03-19 07:25:33 +0900

    feat: hashとLOS追加

--- frontend/src/pages/draw/Draw.tsx
+++ frontend/src/pages/draw/Draw.tsx
@@ -8,52 +8,66 @@ const Draw: React.FC = () => {
         "y": number,
     }
 
-    let aligns = [
+    const HEIGHT = window.innerHeight;
+    const WIDTH = window.innerWidth;
+
+    const toPercentX = (x: number) => {
+        return Math.floor(x / WIDTH * 100)
+    }
+
+    
+    const toPercentY = (y: number) => {
+        return Math.floor(y / HEIGHT * 100)
+    }
+
+    // 縦横それぞれ、100%分の何％かを保存するようにする
+
+    let aligns: Align[] = [
         // TODO: default値を作成
         // TODO: backendから取得するようにする
         {
-            "x": 100,
-            "y": 400,
+            "x": 10,
+            "y": 65,
         },
         {
-            "x": 500,
-            "y": 700,
+            "x": 30,
+            "y": 65,
         },
         {
-            "x": 300,
-            "y": 400,
+            "x": 34,
+            "y": 65,
         },
         {
-            "x": 400,
-            "y": 400,
+            "x": 34,
+            "y": 73,
         },
         {
-            "x": 500,
-            "y": 400,
+            "x": 34,
+            "y": 81,
         },
         {
-            "x": 600,
-            "y": 400,
+            "x": 34,
+            "y": 89,
         },
         {
-            "x": 700,
-            "y": 400,
+            "x": 38,
+            "y": 65,
         },
         {
-            "x": 800,
-            "y": 400,
+            "x": 42,
+            "y": 65,
         },
         {
-            "x": 500,
-            "y": 600,
+            "x": 50,
+            "y": 70,
         },
         {
-            "x": 500,
-            "y": 500,
+            "x": 80,
+            "y": 65,
         },
         {
-            "x": 1100,
-            "y": 500,
+            "x": 26,
+            "y": 65,
         },
     ]
 
@@ -62,28 +76,102 @@ const Draw: React.FC = () => {
     for (let i = 0; i < 11; i++) {
         circles.push(
             <Circle
-                x={aligns[i].x}
-                y={aligns[i].y}
-                radius={30}
+                x={aligns[i].x * WIDTH / 100}
+                y={aligns[i].y * HEIGHT / 100}
+                radius={WIDTH / 60}
                 fill="black"
                 draggable
+                onDragMove={(e: DragEvent<HTMLDivElement>) => {
+                    writeLines(e.target.attrs)
+                }}
                 onDragEnd={(e: DragEvent<HTMLDivElement>) => {
-                    aligns[i].x = e.target.attrs.x
-                    aligns[i].y = e.target.attrs.y
+                    aligns[i].x = toPercentX(e.target.attrs.x)
+                    aligns[i].y = toPercentY(e.target.attrs.y)
                 }}
                 key={i}
             />
         )
     }
 
+    let sameHeightLines = []
+    let sameWidthLines = []
+
+    const writeLines = (attrs) => {
+        sameHeightLines = []
+        sameWidthLines = []
+        aligns.map((align, index) => {
+            if (align.x == toPercentX(attrs.x)) {
+                sameWidthLines.push(<Line
+                    x={0}
+                    y={0}
+                    points={[attrs.x, 0, attrs.x, attrs.y, attrs.x, HEIGHT]}
+                    stroke='black'
+                    tension={0.5}
+                    closed
+                />)
+            }
+
+            if (align.y == toPercentY(attrs.y)) {
+                sameHeightLines.push(<Line
+                    x={0}
+                    y={0}
+                    points={[0, attrs.y, attrs.x, attrs.y, WIDTH, attrs.y]}
+                    stroke='black'
+                    tension={0.5}
+                    closed
+                />)
+            }
+        })
+    }
+
+
     return (
         <div>
             <h2>Draw</h2>
-            <Stage width={window.innerWidth} height={window.innerHeight}>
-                <Layer>
-                    {circles}
-                </Layer>
-            </Stage>
+            <div>
+                <div>
+                    <Stage width={WIDTH} height={HEIGHT}>
+                        <Layer>
+                            {circles}
+                            <Rect
+                                x={0}
+                                y={0}
+                                height={HEIGHT}
+                                width={WIDTH}
+                                stroke="black"
+                                strokeWidth={10}
+            
+                            />
+                            <Line
+                                // LOS
+                                x={0}
+                                y={0}
+                                points={[0, HEIGHT * 0.65, 100, HEIGHT * 0.65, WIDTH, HEIGHT * 0.65]}
+                                tension={0.5}
+                                stroke="black"
+                            />
+                            <Line
+                                // HASH LEFT
+                                x={0}
+                                y={0}
+                                points={[WIDTH / 3, 0, WIDTH / 3, 100, WIDTH / 3, HEIGHT]}
+                                tension={0.5}
+                                stroke="black"
+                            />
+                            <Line
+                                // HASH RIGHT
+                                x={0}
+                                y={0}
+                                points={[WIDTH / 3 * 2, 0, WIDTH / 3 * 2, 100, WIDTH / 3 * 2, HEIGHT]}
+                                tension={0.5}
+                                stroke="black"
+                            />
+                            {sameHeightLines}
+                            {sameWidthLines}
+                        </Layer>
+                    </Stage>
+                </div>
+            </div>
         </div>
     );
 };
